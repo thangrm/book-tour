4f9cd814fbbda31dca751ba521d4c0407caa43a4
Hoàng Văn Thắng <hoangthang.rm@gmail.com>
2021-12-08 17:14:29 +0700

    add Slide Tour and Traditional Cultural - HomePage

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $(".owl-carousel").owlCarousel({
+    $("#slideDestinations").owlCarousel({
         loop: true,
         margin: 1,
         responsiveClass: true,
@@ -18,6 +18,48 @@ $(document).ready(function() {
             },
         }
     });
+
+    $("#slideTours").owlCarousel({
+        loop: true,
+        margin: 1,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+            },
+            992: {
+                items: 2,
+            },
+            1200: {
+                items: 3,
+            },
+        }
+    });
+
+
+    $("#slideCultural").owlCarousel({
+        loop: true,
+        margin: 1,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+            },
+            992: {
+                items: 2,
+            },
+            1200: {
+                items: 3,
+            },
+        }
+    });
+
     $('.next-slide').click(function(event) {
         let slideID = event.target.getAttribute('data-target');
         $(slideID).trigger('next.owl.carousel');
