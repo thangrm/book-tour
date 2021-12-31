6199fcc4fa015d3b686bec7922c16b152fcfdebe
thangrm <hoangthangrm@gmail.com>
2021-12-31 13:43:28 +0700

    list of faqs

--- database/migrations/2021_12_21_093040_create_faqs_table.php
+++ database/migrations/2021_12_21_093040_create_faqs_table.php
@@ -15,7 +15,7 @@ class CreateFaqsTable extends Migration
     {
         Schema::create('faqs', function (Blueprint $table) {
             $table->integer('id', true);
-            $table->integer('tour_id')->nullable()->index('tour_id');
+            $table->integer('tour_id')->index('tour_id');
             $table->string('question')->unique();
             $table->string('answer');
             $table->tinyInteger('status')->default(1)->comment('1: active, 2: inactive');
