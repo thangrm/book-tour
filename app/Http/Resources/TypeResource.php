1f685bcf2c3d976bc1050895fe77065c4f7ac8e3
Hoàng Văn Thắng <hoangthang.rm@gmail.com>
2022-02-08 14:52:25 +0700

    api for homepage

--- app/Http/Resources/TypeResource.php
+++ app/Http/Resources/TypeResource.php
@@ -2,23 +2,18 @@
 
 namespace App\Http\Resources;
 
-use App\Models\Place;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ItineraryResource extends JsonResource
+class TypeResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
-        return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'places' => PlaceResource::collection($this->whenLoaded('places')),
-        ];
+        return parent::toArray($request);
     }
 }
