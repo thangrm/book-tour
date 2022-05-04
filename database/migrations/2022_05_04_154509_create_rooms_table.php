6b84e6103194571012dc1e9056d4ff22dad1d213
thangrm <hoangthang.rm@gmail.com>
2022-05-04 23:06:02 +0700

    update migration

---
+++ database/migrations/2022_05_04_154509_create_rooms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->integer('tour_id');
+            $table->string('name', 150);
+            $table->float('price', 10, 0);
+            $table->unique(['tour_id', 'name']);
+            $table->foreign('tour_id')->references('id')->on('tours')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
---
+++ database/migrations/2022_05_04_155923_create_coupons_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->float('discount_code', 10, 0);
+            $table->float('discount', 10, 0);
+            $table->string('name', 20);
+            $table->integer('number')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+}
